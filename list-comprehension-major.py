5473ab5ed7c24ba5de0778bb30516ead51b535be
Ruan Pablo Moreira Pereira <48455463+ORuan@users.noreply.github.com>
2021-03-18 08:33:40 -0300

    Create list-comprehension-major.py

---
+++ list-comprehension-major.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+print('init: ', datetime.now())
+if __name__ == '__main__':
+    #return object list
+    pars = [ value for value in range(10000000) if value % 2 == 0 >
+    print(pars)
+print('finish: ', datetime.now())
